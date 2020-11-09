7736092696799244d3b927fbedf2e46dc5ca2b96
Yanis2020 <amimeuryanis@gmail.com>
2020-11-09 00:33:58 -0600

    update commit

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -12,17 +12,24 @@ public class FindLowestDifference {
 
 
     }
-    public static void cal (){
+
+    public static void cal() {
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
-        int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
+        // int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
 
-        int tmp;
-        int finalResult=0;
-        for (int i=0; i<=array1.length;i++){
-            tmp=array1[i+1];
-            finalResult = array1 [i] - tmp;
+        int tmp, tmp2;
+        int finalResult = array1[0];
 
-
+        for (int i = 0; i < array1.length -1; i++) {
+            tmp = array1[i];
+            for (int j = 0; j < array1.length -1; j++) {
+                //tmp = array1[j];
+                tmp2 = array1[j + 1];
+                int temp2;
+                if ((temp2 = (tmp - tmp2)) > 0) {
+                    finalResult = temp2;
+                }
+            }
         }
         System.out.println("The lowest difference is " + finalResult);
 
--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -15,6 +15,15 @@ public class LowestNumber {
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
 
         //find lowest number from the array
+        int tmp;
+        int finalResult= array[0];
+        for (int i=1; i<array.length; i++){
+            tmp = array[i];
+            if(finalResult>tmp) {
+                finalResult = tmp;
+            }
+        }
+        System.out.println("The lowest number from the array is "+ finalResult);
 
         ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
         List<String> lowestValue = new ArrayList<String>();
